d1ef596751d0ea73b4c5c05f318913c30696ae3a
tuhindewan <tuhinsshadow@gmail.com>
2021-04-05 17:33:45 +0600

    CAAB-60: Configure admin-lte and font awesome

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -13,7 +13,7 @@
         <img src="{{ asset('img/profile.png') }}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-        <a href="#" class="d-block">Super Admin</a>
+        <a href="#" class="d-block">{{ Auth::user()->name }}</a>
         </div>
     </div>
 
@@ -32,40 +32,54 @@
     <!-- Sidebar Menu -->
     <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-        <!-- Add icons to the links using the .nav-icon class
-            with font-awesome or any other icon font library -->
-        <li class="nav-item menu-open">
-            <a href="#" class="nav-link active">
-            <i class="nav-icon fas fa-tachometer-alt"></i>
-            <p>
-                Starter Pages
-                <i class="right fas fa-angle-left"></i>
-            </p>
-            </a>
-            <ul class="nav nav-treeview">
             <li class="nav-item">
-                <a href="#" class="nav-link active">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Active Page</p>
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-tachometer-alt text-blue"></i>
+                    <p>Dashboard</p>
                 </a>
             </li>
             <li class="nav-item">
                 <a href="#" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Inactive Page</p>
+                <i class="nav-icon fas fa-cogs text-green"></i>
+                <p>
+                    Management
+                    <i class="right fas fa-angle-left"></i>
+                </p>
+                </a>
+                <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="#" class="nav-link \">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Active Page</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Inactive Page</p>
+                    </a>
+                </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-user text-yellow"></i>
+                    <p>Profile</p>
                 </a>
             </li>
-            </ul>
-        </li>
-        <li class="nav-item">
-            <a href="#" class="nav-link">
-            <i class="nav-icon fas fa-th"></i>
-            <p>
-                Simple Link
-                <span class="right badge badge-danger">New</span>
-            </p>
-            </a>
-        </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                    <i class="nav-icon fas fa-power-off text-red"></i>
+
+                    <p>{{ __('Logout') }}</p>
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </li>
         </ul>
     </nav>
     <!-- /.sidebar-menu -->
