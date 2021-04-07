115411899ec9e52c4040db1015f46ad2f7ea03bc
tuhindewan <tuhinsshadow@gmail.com>
2021-04-07 11:03:11 +0600

    CAAB-47: User creation Data try and catch handling 	 - Employee model

--- database/migrations/2021_04_06_090224_create_employees_table.php
+++ database/migrations/2021_04_06_090224_create_employees_table.php
@@ -20,6 +20,7 @@ class CreateEmployeesTable extends Migration
             $table->string('mobile');
             $table->string('signature');
             $table->string('seal');
+            $table->boolean('status')->default(1);
             $table->foreignId('user_id')->constrained()
                     ->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
